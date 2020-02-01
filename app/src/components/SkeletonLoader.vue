b0132e43457e6083986f765a20ae6828370fbc87
Mustafa Refaey <mustafarefaey@gmail.com>
2020-02-01 15:29:57 +0200

    add SkeletonLoader component

---
+++ app/src/components/SkeletonLoader.vue
@@ -0,0 +1,15 @@
+<template>
+	<v-sheet :color="`grey ${theme.isDark ? 'darken-2' : 'lighten-4'}`">
+		<v-skeleton-loader class="mx-auto" type="image"></v-skeleton-loader>
+	</v-sheet>
+</template>
+
+<script>
+	export default {
+		name: "SkeletonLoader",
+		// Vuetify components provide
+		// a theme variable that is
+		// used to determine dark
+		inject: ["theme"]
+	};
+</script>
