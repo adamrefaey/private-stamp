26386bf6cdf48483a0666cd8369e0b3b75e55d88
Mustafa Refaey <mustafarefaey@gmail.com>
2020-02-01 15:28:13 +0200

    add CopyableText component

---
+++ app/src/components/CopyableText.vue
@@ -0,0 +1,40 @@
+<template>
+	<kbd class="cyan darken-3">
+		<v-tooltip
+			bottom
+			v-model="showToolTip"
+			:open-on-hover="false"
+			:internal-activator="true"
+		>
+			<template v-slot:activator="{ on }">
+				<span @click="doCopy" v-on="on">{{ text }}</span>
+			</template>
+			<span v-show="showToolTip">Copied!</span>
+		</v-tooltip>
+		<v-btn small color="accent-4" right icon @click="doCopy">
+			<v-icon small>mdi-content-copy</v-icon>
+		</v-btn></kbd
+	>
+</template>
+
+<script>
+	export default {
+		name: "CopyableText",
+		methods: {
+			doCopy: function() {
+				this.showToolTip = true;
+				this.$copyText(this.text);
+				setTimeout(() => {
+					this.showToolTip = false;
+				}, 1000);
+			}
+		},
+		data: () => ({
+			showToolTip: false
+		}),
+		props: ["text"]
+	};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
