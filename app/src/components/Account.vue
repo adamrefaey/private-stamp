8f658e2e526bbc571e28f6cb67cbce073ee1ec3a
Mustafa Refaey <mustafarefaey@gmail.com>
2020-02-01 15:29:02 +0200

    update Account component

--- app/src/components/Account.vue
+++ app/src/components/Account.vue
@@ -1,17 +1,26 @@
 <template>
-	<div>
-		<h4>Your address: {{ activeAccount }}</h4>
-		<h4>Balance: {{ activeBalance }} Wei</h4>
+	<div right class="secondary--text">
+		<b> Your address: </b> <CopyableText :text="activeAccount" />
+		<v-divider class="mx-4" vertical></v-divider>
+		<b> Balance: </b
+		><v-chip class="pa-2 cyan darken-3" label
+			>{{ activeBalance / Math.pow(10, 18) }}
+			<b class="ml-2">Eth</b></v-chip
+		>
 	</div>
 </template>
 
 <script>
 	import { mapGetters } from "vuex";
+	import CopyableText from "./CopyableText";
 
 	export default {
 		name: "Account",
 		computed: {
 			...mapGetters("accounts", ["activeAccount", "activeBalance"])
+		},
+		components: {
+			CopyableText
 		}
 	};
 </script>
