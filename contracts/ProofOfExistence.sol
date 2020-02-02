9af5cee6a070286ebc55353ae09052b54253095c
Mustafa Refaey <mustafarefaey@gmail.com>
2020-02-02 05:12:11 +0200

    rename hashUploader to stampper

--- contracts/ProofOfExistence.sol
+++ contracts/ProofOfExistence.sol
@@ -19,7 +19,7 @@ contract ProofOfExistence {
 
     /// an event to be emitted when a new hash has been added
     event LogAdditionEvent(
-        address indexed hashUploader,
+        address indexed stampper,
         uint256 blockNumber,
         string hash
     );
@@ -59,16 +59,17 @@ contract ProofOfExistence {
     }
 
     /// @notice Verifies if the hash exists
-    /// @param hashUploader The address of the hash uploader
+    /// @param stampper The address of the stampper
     /// @param hash The hash to be stored
     /// @return the block number of a hash if it exists in the contract's state
     /// or returns 0
-    function verifyHash(address hashUploader, string memory hash)
+    function verifyHash(address stampper, string memory hash)
         public
         view
         stopInEmergency
         returns (uint256)
     {
-        return hashes[hashUploader][hash];
+        return hashes[stampper][hash];
+    }
     }
 }
