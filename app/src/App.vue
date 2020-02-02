80a97597a31e35066798fd00d303e351b5fd66d7
Mustafa Refaey <mustafarefaey@gmail.com>
2020-02-02 11:07:30 +0200

    refactor App component

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,26 +1,39 @@
 <template>
 	<v-app id="app">
 		<v-content v-if="isDrizzleInitialized && appIsUpToDate">
-			<v-app-bar color="surface" dense class="elevation-5">
-				<v-toolbar-title
-					><v-btn
-						color="cyan darken-3"
-						target="_blank"
-						href="https://github.com/mustafarefaey/PrivateStamp"
-						>PrivateStamp</v-btn
-					></v-toolbar-title
-				>
-
-				<v-spacer></v-spacer>
-				<Account class="text-right" />
-			</v-app-bar>
 			<v-container fluid>
 				<Notifications />
-				<v-row wrap>
-					<v-col cols="6">
+				<v-row color="surface" dense class="elevation-5">
+					<v-col xs="12" md="3">
+						<v-btn
+							class="ma-3"
+							left
+							color="cyan darken-3"
+							target="_blank"
+							href="https://github.com/mustafarefaey/PrivateStamp"
+							>PrivateStamp</v-btn
+						>
+					</v-col>
+
+					<v-col xs="12" md="9" class="text-right">
+						<div class="d-inline-block text-left ma-3">
+							<b> Your address: </b>
+							<CopyableText :text="activeAccount" />
+						</div>
+						<div class="d-inline-block text-left ma-3">
+							<b class=""> Balance: </b
+							><v-chip class="cyan darken-3" label
+								>{{ activeBalance / Math.pow(10, 18) }}
+								<b class="ml-2">Eth</b></v-chip
+							>
+						</div>
+					</v-col>
+				</v-row>
+				<v-row>
+					<v-col xs="12" md="6">
 						<AddFile />
 					</v-col>
-					<v-col cols="6">
+					<v-col xs="12" md="6">
 						<VerifyHash />
 					</v-col>
 				</v-row>
@@ -36,25 +49,28 @@
 <script>
 	import { mapGetters } from "vuex";
 	import Notifications from "./components/Notifications";
-	import Account from "./components/Account";
+	// import Account from "./components/Account";
 	import VerifyHash from "./components/VerifyHash";
 	import AddFile from "./components/AddFile";
 	import SkeletonLoader from "./components/SkeletonLoader";
 	import AppOutdated from "./components/AppOutdated";
 	import { APP_VERSION } from "./appConfig";
+	import CopyableText from "./components/CopyableText";
 
 	export default {
 		name: "app",
 		components: {
-			Account,
+			// Account,
 			VerifyHash,
 			AddFile,
 			SkeletonLoader,
 			Notifications,
-			AppOutdated
+			AppOutdated,
+			CopyableText
 		},
 		computed: {
-			...mapGetters("drizzle", ["isDrizzleInitialized", "drizzleInstance"])
+			...mapGetters("drizzle", ["isDrizzleInitialized", "drizzleInstance"]),
+			...mapGetters("accounts", ["activeAccount", "activeBalance"])
 		},
 		mounted: function() {
 			if (this.isDrizzleInitialized) {
