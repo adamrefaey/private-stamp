1639bc04e8c01454ba58c4cb44e88e808980d9c5
Mustafa Refaey <mustafarefaey@gmail.com>
2021-04-19 06:06:52 +0200

    refactor ethereum utilities

--- front-end/src/utils/ethereum.ts
+++ front-end/src/utils/ethereum.ts
@@ -1,21 +1,35 @@
-import { ethers } from "ethers";
+import { ethers, providers } from "ethers";
 
-export async function connectToMetamask() {
+export async function connectToMetamask(): Promise<providers.Web3Provider> {
   const ethereum = (window as any).ethereum;
+
   if (!ethereum) {
     throw Error("Metamask extension is missing");
   }
-  let provider;
+
   try {
     await ethereum.enable();
-    provider = new ethers.providers.Web3Provider(ethereum);
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    if ((await provider.getNetwork()).name !== "rinkeby") {
+      throw Error(
+        "Please make sure that Metamask is connected to Rinkeby network"
+      );
+    }
+
+    return provider;
   } catch (err) {
     throw Error("Please accept Metamask connection to this app");
   }
+}
 
-  if ((await provider.getNetwork()).name !== "rinkeby") {
-    throw Error(
-      "Please make sure that Metamask is connected to Rinkeby network"
-    );
-  }
+export function getConnectedAccount(
+  web3Provider: providers.Web3Provider
+): providers.JsonRpcSigner {
+  return web3Provider?.getSigner();
+}
+
+export async function getConnectedAccountAddress(
+  web3Provider: providers.Web3Provider
+): Promise<string> {
+  return await web3Provider?.getSigner().getAddress();
 }
