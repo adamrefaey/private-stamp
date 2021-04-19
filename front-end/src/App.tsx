c9209c167ea3a15a0efb4c28b3356cc9d7e2be23
Mustafa Refaey <mustafarefaey@gmail.com>
2021-04-19 06:08:14 +0200

    refactor app

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,51 +1,22 @@
-import React, { useEffect } from "react";
-import {
-  ChakraProvider,
-  theme,
-  useToast,
-  Flex,
-  SimpleGrid,
-} from "@chakra-ui/react";
-import { connectToMetamask } from "./utils/ethereum";
+import React from "react";
+import { ChakraProvider, theme, Flex, SimpleGrid } from "@chakra-ui/react";
 import AppHeader from "./components/AppHeader";
 import StampContainer from "./components/stamp";
 import VerifyContainer from "./components/verify";
+import MetamaskProvider from "./components/MetamaskProvider";
 
 export const App: React.FC = () => {
-  const toast = useToast();
-
-  useEffect(() => {
-    (async () => {
-      try {
-        await connectToMetamask();
-        toast({
-          position: "top-left",
-          title: "Metamask connected successfully.",
-          status: "success",
-          duration: 2000,
-          // isClosable: true,
-        });
-      } catch (err) {
-        toast({
-          position: "top-left",
-          title: err.message,
-          status: "error",
-          duration: null,
-          isClosable: true,
-        });
-      }
-    })();
-  }, [toast]);
-
   return (
     <ChakraProvider theme={theme}>
-      <Flex minH="100vh" flexDirection="column" px="10" py="5">
-        <AppHeader />
-        <SimpleGrid columns={{ base: 1, md: 2 }} spacing="5">
-          <StampContainer />
-          <VerifyContainer />
-        </SimpleGrid>
-      </Flex>
+      <MetamaskProvider>
+        <Flex minH="100vh" flexDirection="column" px="10" py="5">
+          <AppHeader />
+          <SimpleGrid columns={{ base: 1, md: 2 }} spacing="5">
+            <StampContainer />
+            <VerifyContainer />
+          </SimpleGrid>
+        </Flex>
+      </MetamaskProvider>
     </ChakraProvider>
   );
 };
