8a0062a85c28ba6fae06368200ecdbce04a97847
Mustafa Refaey <mustafarefaey@gmail.com>
2021-04-21 07:36:26 +0200

    add hardhat-abi-exporter

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("hardhat-abi-exporter");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -27,4 +28,10 @@ module.exports = {
   //   },
   // },
   solidity: "0.7.6",
+  abiExporter: {
+    path: "./abi",
+    clear: true,
+    flat: true,
+    spacing: 2,
+  },
 };
