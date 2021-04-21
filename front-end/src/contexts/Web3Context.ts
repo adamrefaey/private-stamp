99784c05c844786519bf6b369e4007ccf6bcc512
Mustafa Refaey <mustafarefaey@gmail.com>
2021-04-21 07:39:01 +0200

    add contract context

--- front-end/src/contexts/Web3Context.ts
+++ front-end/src/contexts/Web3Context.ts
@@ -1,12 +1,14 @@
-import { providers } from "ethers";
+import { ethers, providers } from "ethers";
 import { createContext } from "react";
 
 interface Web3ContextProps {
   web3Provider?: providers.Web3Provider;
+  contract?: ethers.Contract;
 }
 
 const Web3Context = createContext<Web3ContextProps>({
   web3Provider: undefined,
+  contract: undefined,
 });
 
 export default Web3Context;
